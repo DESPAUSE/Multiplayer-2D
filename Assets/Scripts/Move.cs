7c35f665daa33dce6f51aa341e76b275aac5ea0d
Aluno <aluno@LI.LOCAL>
2022-03-29 19:34:54 -0300

    Commit

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class Move : MonoBehaviour
+public class Move : MonoBehaviourPun, IPunObservable
 {
     public float speed;
  
     Rigidbody2D rb;
     Animator animator;
     PhotonView view;
+    SpriteRenderer render;
 
     float hor, ver;
     Vector2 direction;
@@ -20,6 +21,7 @@ public class Move : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         view = GetComponent<PhotonView>();
+        render = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -34,10 +36,11 @@ public class Move : MonoBehaviour
 
         if (hor > 0)
         {
-            transform.localScale = new Vector3(1, 1, 1);
-        } else if (hor < 0)
+            render.flipX = false;
+        }
+        else if (hor < 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            render.flipX = true;
         }
     }
 
@@ -45,5 +48,18 @@ public class Move : MonoBehaviour
     {
         rb.velocity = direction;
         animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(render.flipX);
+        }
+        else
+        {
+            render.flipX = (bool)stream.ReceiveNext();
+        }
+
     }
 }
